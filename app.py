983de5a2012bf88666438a2b79d6751f5020eb4b
Pedro Rodrigues <pedrovitorlima@gmail.com>
2024-11-30 19:58:29 +1100

    create stable version of broker

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import os
 
 load_dotenv()
 
-MQTT_BROKER = os.getenv("MQTT_BROKER")
+MQTT_BROKER = os.getenv("BROKER_NAME")
 MQTT_PORT = 1883
 MQTT_TOPIC = "prod/sensor_wormhole"
-MQTT_USER = os.getenv("MQTT_USER")
-MQTT_PASSWORD = os.getenv("MQTT_BROKER_PASSWORD")
+MQTT_USER = os.getenv("BROKER_USER")
+MQTT_PASSWORD = os.getenv("BROKER_PASSWORD")
+
+print(f'setting user {MQTT_USER} and pass {MQTT_PASSWORD}')
+print(f'connecting with {MQTT_BROKER} broker')
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
@@ -27,8 +30,8 @@ mqttc.on_message = on_message
 
 mqttc.connect(MQTT_BROKER, MQTT_PORT, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-mqttc.loop_forever()
+# # Blocking call that processes network traffic, dispatches callbacks and
+# # handles reconnecting.
+# # Other loop*() functions are available that give a threaded interface and a
+# # manual interface.
+mqttc.loop_start()
---
+++ produce_for_test.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+import os
+
+# load_dotenv()
+
+MQTT_BROKER = "localhost"
+MQTT_PORT = 1883
+MQTT_TOPIC = "prod/sensor_wormhole"
+MQTT_USER = "iluvatar"
+MQTT_PASSWORD = os.getenv("MQTT_BROKER_PASSWORD")
+
+print(f'setting user {MQTT_USER} and pass {MQTT_PASSWORD} and opa {os.getenv("ANYTHING")}')
+print(f'connecting with ${MQTT_BROKER} broker')
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+mqttc.connect(MQTT_BROKER, MQTT_PORT, 60)
+mqttc.publish(MQTT_TOPIC, "how's going")
+print('message is sent')
+
+mqttc.loop_forever()
